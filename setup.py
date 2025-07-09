14a00312b2e97bb6b7aef6ed0d21952afc82b2da
gmatilde@ethz.ch <gmatilde@itet-ifa-601.ethz.ch>
2025-07-09 18:06:28 +0200

    reshaping

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="riskaverse_DP",
+    name="CVaR_MDPs",
     version="0.1.0",
     packages=find_packages(),
     install_requires=requirements,  # <- use requirements.txt
